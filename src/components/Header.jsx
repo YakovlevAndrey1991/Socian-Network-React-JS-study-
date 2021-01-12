69c31e45219e30447c12e6fff2627ba4c65a1283
Andrey <example@gmail.com>
2021-01-12 18:02:42 +0300

    lesson12

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/5e17558f848f82e664c09d67_logo-dark.svg"
+                alt=""/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src="https://alpindustria.ru/i/tour/programms/596_1800.jpg" alt=""/>
+            <div>ava+description</div>
+            <div>My posts
+                <div>New Post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
