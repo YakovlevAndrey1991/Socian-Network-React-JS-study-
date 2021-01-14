2d17d59e41faa67b7e43d8661d2cbb3d145f834e
Andrey <example@gmail.com>
2021-01-14 14:52:25 +0300

    lesson 14 add css-module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
+import s from './Header.module.css';
+
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img
                 src="https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/5e17558f848f82e664c09d67_logo-dark.svg"
                 alt=""/>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,15 @@
+import s from './Profile.module.css';
+
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <img src="https://alpindustria.ru/i/tour/programms/596_1800.jpg" alt=""/>
             <div>ava+description</div>
             <div>My posts
                 <div>New Post</div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
+                <div className={s.posts}>
+                    <div className={s.item}>post 1</div>
+                    <div className={s.item}>post 2</div>
                 </div>
             </div>
         </div>
