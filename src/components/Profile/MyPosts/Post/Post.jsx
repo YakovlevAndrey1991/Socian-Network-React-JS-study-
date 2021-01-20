69ace53bfac825fb941d205e0a730c27db0e3416
Andrey <example@gmail.com>
2021-01-20 12:04:00 +0300

    l15 add props

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img
                 src="https://dthezntil550i.cloudfront.net/kg/latest/kg1802132010216500004834729/1280_960/557d644f-12f3-49e1-bb66-23c16400540d.png"
                 alt=""/>
-            post 1
+            { props.message }
             <div>
                 <span>Like</span>
             </div>
