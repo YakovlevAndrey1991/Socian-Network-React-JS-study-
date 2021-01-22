c870909608b78661504d216ce4da36c8e969ba1c
Andrey <example@gmail.com>
2021-01-22 12:24:04 +0300

    lesson 27 change component to render

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
 
+
 const App = (props) => {
     return (
         <BrowserRouter>
@@ -17,11 +18,13 @@ const App = (props) => {
                 <Navbar/>
 
                 <div className='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
-                    <Route path='/news' component={News}/>
-                    <Route path='/music' component={Music}/>
-                    <Route path='/settings' component={Settings}/>
+
+                    <Route path='/dialogs' render={ () => <Dialogs/> }/>
+                    <Route path='/profile' render={ () => <Profile/> }/>
+                    <Route path='/news'    render={ () => <News/> }/>
+                    <Route path='/music'   render={ () => <Music/> }/>
+                    <Route path='/settings' render={ () => <Settings/> }/>
+
                 </div>
 
             </div>
