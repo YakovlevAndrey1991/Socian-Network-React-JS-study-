42be5a640b47a7603656e1821859b5800edab978
Andrey <example@gmail.com>
2021-01-22 12:11:14 +0300

    lesson 26 refactoring

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,10 +4,10 @@ import Post from "./Post/Post";
 const MyPosts = () => {
 
     let posts = [
-        {id: 1, message: 'Hi, how are you?', likesCount: 1223232},
-        {id: 2, message: "It's my first post", likesCount: 11},
-        {id: 3, message: "It's my second post", likesCount: 11},
-        {id: 4, message: "It's my third post", likesCount: 11},
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: "It's my first post", likesCount: 1},
+        {id: 3, message: "It's my second post", likesCount: 7},
+        {id: 4, message: "It's my third post", likesCount: 5},
 
     ];
 
